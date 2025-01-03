8d7e599e1cc2a5e04de6757761b72159eaca25cb
Dylo98 <dawid.dyl@microsoft.wsei.edu.pl>
2025-01-03 15:38:37 +0100

    map implementation, finding the user's location

--- script.js
+++ script.js
@@ -11,15 +11,47 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition();
+  }
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
+        }
+      );
+    }
+  }
+  _loadMap(position) {
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
+
+    this.#map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm() {}
+  _newWorkout() {}
+  _showForm() {}
+  _toggleElevationField() {}
+}
+
+const app = new App();
+
 // Leaflet
-var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
-
-L.marker([51.5, -0.09])
-  .addTo(map)
-  .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-  .openPopup();
+// L.marker([51.5, -0.09])
+//   .addTo(map)
+//   .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+//   .openPopup();
